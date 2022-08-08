c0220bf685330194365296320595c72004c8536a
Yielding <90570076+YieldingExploiter@users.noreply.github.com>
2022-08-08 22:41:58 +0200

    feat: add very basic aead_chacha_poly defs if anyone actually uses this, feel free to submit a pr

---
+++ plc/aead_chacha_poly.d.ts
@@ -0,0 +1,7 @@
+type aead_chacha_poly = {
+  poly_keygen: (key:string, nonce:string) => string;
+  encrypt: (aad: any, key: any, iv: any, constant: any, plain: any) => any;
+  decrypt: (aad: any, key: any, iv: any, constant: any, plain: any) => any;
+}
+declare const aead_chacha_poly: aead_chacha_poly;
+export = aead_chacha_poly
