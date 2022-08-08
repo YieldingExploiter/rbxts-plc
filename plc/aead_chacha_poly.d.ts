8a52a658906882a7e9ec64a36d939204b77e11f2
YieldingExploiter <yieldingexploiter@users.noreply.github.com>
2022-08-09 01:17:04 +0200

    feat: :label: Add type defs for chacha poly

--- plc/aead_chacha_poly.d.ts
+++ plc/aead_chacha_poly.d.ts
@@ -1,3 +1,4 @@
+/** @deprecated VERY LIKELY DOES NOT WORK IN ALL RBXTS ENVS */
 type aead_chacha_poly = {
   poly_keygen: (key:string, nonce:string) => string;
   encrypt: (aad: any, key: any, iv: any, constant: any, plain: any) => any;
